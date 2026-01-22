add4c66dd3b1671bc6667826687edbf1c96198b9
Jean Mathieu Lerda <jeanmathieulerda@MacBook-Pro-de-Jean.local>
2026-01-22 11:33:42 +0100

    Mise en place vue.js tailwind typscript

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="h-screen flex items-center justify-center bg-black text-cyan-400 text-3xl font-bold">
+    Projet Front Vue 3 🚀
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+/* CSS spécifique à ce composant si nécessaire */
+</style>
