958a05244971afa90b42676c99cfd2d93558e1e9
eufopla <emmanuelaprofir@gmail.com>
2026-02-06 08:58:21 +0100

    Add update and hard delete methods to CommunityPostController and corresponding routes

--- network/app/Http/Controllers/Community/CommunityPostController.php
+++ network/app/Http/Controllers/Community/CommunityPostController.php
@@ -6,4 +6,10 @@ class CommunityPostController extends Controller {
     public function createPost(Request $request) {
         // Logic to create a new community post
     }
+    public function updatePost(Request $request) {
+        // Logic to update a community post
+    }
+    public function hardDeletePost(Request $request) {
+        // Logic to hard delete a community post
+    }
 }
--- network/routes/communityPost.php
+++ network/routes/communityPost.php
@@ -4,3 +4,11 @@ $router->get('community/post/create', [
     'middleware' => [],
     'uses' => 'Community\CommunityPostController@createPost',
 ]);
+$router->get('community/post/update', [
+    'middleware' => [],
+    'uses' => 'Community\CommunityPostController@updatePost',
+]);
+$router->get('community/post/harddelete', [
+    'middleware' => [],
+    'uses' => 'Community\CommunityPostController@hardDeletePost',
+]);
